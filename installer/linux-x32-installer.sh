a4ac6b8ebd7fa1a74165b89271bb3c5eff6dd4d6
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-01-29 14:42:25 -0500

    Create linux-x32-installer.sh

---
+++ installer/linux-x32-installer.sh
@@ -0,0 +1,5 @@
+echo "CS:GO Custom Language Installer"
+echo -n "csgo_*.txt file: "
+read ans
+cp -f $ans "~/.steam/steamapps/common/Counter-Strike Global Offensive/csgo/resource/"
+# not tested, I don't have access to a linux pc
