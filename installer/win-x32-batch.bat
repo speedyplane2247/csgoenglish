76fcb260874cd85121520decac505e4927ce1230
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-01-29 14:35:54 -0500

    Update win-x32-batch.bat

--- installer/win-x32-batch.bat
+++ installer/win-x32-batch.bat
@@ -1,3 +1,33 @@
 @echo off
 title Custom CS:GO Language Updater
-rem wip
+cls
+if %1=help goto :help
+if %1=customcsgopath goto :customcsgopath
+:main
+echo Path to File:
+set /p csgofile=Path:
+echo Name of language
+set /p language=Name:
+copy %csgofile% "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\resource\templang.xlf"
+cd "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\resource\"
+copy templang.xlf csgo_%language%.txt
+echo Completed.
+goto :exit
+:exit
+exit
+:help
+echo "help | loads this command"
+echo "customcsgopath | use a custom csgo path"
+goto :exit
+:customcsgopath
+echo Path to File:
+set /p csgofile=Path:
+echo Name of language
+set /p language=Name:
+echo CS:GO path
+set /p csgopath=Path:
+copy %csgofile% %csgopath%\templang.xlf
+cd %csgopath%
+copy templang.xlf csgo_%language%.txt
+echo Completed.
+goto :exit
