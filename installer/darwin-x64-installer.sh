acff315026dcba73d308d6b7fa29321a1faa00f1
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-01-29 14:45:52 -0500

    1.0

---
+++ installer/darwin-x64-installer.sh
@@ -0,0 +1,5 @@
+echo "CS:GO Custom Language Installer"
+echo -n "csgo_*.txt file: "
+read ans
+cp -f $ans "/Users/student/Library/Application Support/Steam/steamapps/common/Counter-Strike Global Offensive/csgo/resource/"
+# just modified for mac directories
